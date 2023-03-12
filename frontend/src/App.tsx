b4a5410e7f6879aea1360864834bbc05321e3d4c
Edy Espinal <edyespinal@gmail.com>
2023-03-12 23:36:36 +0100

    chore: add initial configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cardo Health</h1>
+
+      <p className="read-the-docs">Technical test</p>
+    </div>
+  )
+}
+
+export default App
