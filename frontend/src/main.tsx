128bba5b9808132a38d1ae1d70a1802d489d49f2
edyespinal <edy.espinal@vtex.com.br>
2023-03-14 23:57:51 +0100

    chore: add theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,25 @@
+import { createEmotionCache, MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
 import './index.css'
+import { theme } from './theme'
+
+const emotionCache = createEmotionCache({
+  key: 'mantine',
+  prepend: false,
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={theme}
+      withGlobalStyles
+      withNormalizeCSS
+      emotionCache={emotionCache}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )
