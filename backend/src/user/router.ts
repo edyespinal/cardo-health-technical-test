b68313d0f34bd3a94cc3c0dbff0a91a5bed1bec3
edyespinal <edy.espinal@vtex.com.br>
2023-03-14 23:59:52 +0100

    feat: add createUser mutation

--- backend/src/user/router.ts
+++ backend/src/user/router.ts
@@ -13,4 +13,34 @@ export const userRouter = router({
 
     return foundUser
   }),
+  createUser: publicProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        password: z
+          .string()
+          .min(6, { message: 'Password must be at least 6 characters long.' })
+          .max(128),
+        firstName: z
+          .string()
+          .min(3, { message: 'Name must be at least 3 characters long.' }),
+        lastName: z
+          .string()
+          .min(3, { message: 'Last name must be at least 3 characters long.' }),
+      })
+    )
+    .mutation(({ input }) => {
+      const newUser: User = {
+        id: String(users.length + 1),
+        email: input.email,
+        password: input.password,
+        firstName: input.firstName,
+        lastName: input.lastName,
+        books: [],
+      }
+
+      users.push(newUser)
+
+      return newUser
+    }),
 })
