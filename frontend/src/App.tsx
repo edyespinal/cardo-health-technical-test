f7210ae4f8d2ca71bddd0be9276e519091961f96
Edy Espinal <edyespinal@gmail.com>
2023-03-14 09:30:42 +0100

    feat: add tRPC to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,29 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+import { useState } from 'react'
+
 import './App.css'
+import { IndexPage } from './components/pages'
+import { trpc } from './utils/trpc'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Cardo Health</h1>
+  const [queryClient] = useState(() => new QueryClient())
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: 'http://localhost:3000/v1/api',
+        }),
+      ],
+    })
+  )
 
-      <p className="read-the-docs">Technical test</p>
-    </div>
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <IndexPage />
+      </QueryClientProvider>
+    </trpc.Provider>
   )
 }
 
