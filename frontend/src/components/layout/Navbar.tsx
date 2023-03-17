2b5322a4d2d649726e7cf195efa0ee6eeb57fc7b
edyespinal <edy.espinal@vtex.com.br>
2023-03-17 23:26:20 +0100

    chore: update navbar

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-console */
 import { Avatar, Container, Menu, Text, UnstyledButton } from '@mantine/core'
 
+import { useSession } from '../../hooks/useSession'
+
 const Navbar = () => {
+  const { user } = useSession()
+
   return (
     <Container fluid p={0}>
       <div className="flex justify-between py-2 px-8 mb-8 shadow-lg">
-        <h1>CardoBooks</h1>
+        <h1 className="text-brand">CardoBooks</h1>
         <Menu shadow={'md'} width={200}>
           <Menu.Target>
             <UnstyledButton className="drop-shadow-lg">
@@ -15,9 +19,9 @@ const Navbar = () => {
 
           <Menu.Dropdown>
             <Menu.Label>
-              <Text size="sm">{'user?.firstName'}</Text>
+              <Text size="sm">{`${user?.firstName} ${user?.lastName}`} </Text>
             </Menu.Label>
-            <Menu.Item onClick={() => console.log('handleLogout')}>
+            <Menu.Item onClick={() => console.log('TODO: handleLogout')}>
               Logout
             </Menu.Item>
           </Menu.Dropdown>
