5b9cc3259a79442b36ff6cc8d0374173860c9bf8
edyespinal <edy.espinal@vtex.com.br>
2023-03-17 23:22:04 +0100

    chore: remove unused queries

--- backend/src/user/router.ts
+++ backend/src/user/router.ts
@@ -5,14 +5,6 @@ import { User } from './types'
 import { publicProcedure, router } from '../trpc'
 
 export const userRouter = router({
-  getAllUsers: publicProcedure.query<User[]>(() => {
-    return users
-  }),
-  getUser: publicProcedure.input(z.string()).query(({ input }) => {
-    const foundUser = users.find((user) => user.id === input)
-
-    return foundUser
-  }),
   createUser: publicProcedure
     .input(
       z.object({
