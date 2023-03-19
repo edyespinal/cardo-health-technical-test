810fd68991021a0cc0cdabb3b520aa836c99c210
edyespinal <edy.espinal@vtex.com.br>
2023-03-19 13:40:38 +0100

    chore: check if user is signedIn, theme update

---
+++ frontend/src/theme/components.ts
@@ -0,0 +1,11 @@
+import { MantineThemeComponents } from '@mantine/styles/lib/theme/types/MantineTheme'
+
+export const components: MantineThemeComponents = {
+  Button: {
+    defaultProps: {
+      size: 'md',
+      radius: 'xl',
+      uppercase: true,
+    },
+  },
+}
